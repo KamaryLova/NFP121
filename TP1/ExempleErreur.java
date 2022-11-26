87f32d0849314e1e06e5750991463e187f3da6dd
KamaryLova <rakotokamary@gmail.com>
2022-11-26 22:21:51 +0100

    Update Readme

---
+++ TP1/ExempleErreur.java
@@ -0,0 +1,16 @@
+/** Une erreur à la compilation !
+  * Pourquoi ?
+  * @author	Xavier Crégut
+  * @version	1.3
+  */
+public class ExempleErreur {
+
+	/** Méthode principale */
+	public static void main(String[] args) {
+		Point p1 = new Point(2, 1);
+		p1.setX(1);
+		p1.setY(2);
+		p1.afficher();
+		System.out.println();
+	}
+}
